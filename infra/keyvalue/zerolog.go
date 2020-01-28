63b9c4802da9d42a4909f87a22443ec8dc64adc2
ENDOH takanao <djmchl@gmail.com>
2020-01-28 12:54:06 +0900

    op

--- infra/keyvalue/zerolog.go
+++ infra/keyvalue/zerolog.go
@@ -10,37 +10,37 @@ import (
 )
 
 func (p *Key) MarshalZerologObject(e *zerolog.Event) {
-	const op = op + ".Key.MarshalZerologObject"
-	v0, err := json.Marshal(&p)
+	const op = op + ".Key"
+	v, err := json.Marshal(&p)
 	if err != nil {
-		const op = op + ".json.Marshal"
+		const op = op + ".MarshalZerologObject.json.Marshal"
 		err := &errs.Error{Op: op, Code: codes.InvalidArgument, Err: err}
 		slog.Logger().Err(err).Str("op", op).Msg(err.Error())
 		return
 	}
-	e.RawJSON(op+".Key", v0)
+	e.RawJSON(op, v)
 }
 
 func (p *KeyValue) MarshalZerologObject(e *zerolog.Event) {
-	const op = op + ".KeyValue.MarshalZerologObject"
-	v0, err := json.Marshal(&p)
+	const op = op + ".KeyValue"
+	v, err := json.Marshal(&p)
 	if err != nil {
-		const op = op + ".json.Marshal"
+		const op = op + ".MarshalZerologObject.json.Marshal"
 		err := &errs.Error{Op: op, Code: codes.InvalidArgument, Err: err}
 		slog.Logger().Err(err).Str("op", op).Msg(err.Error())
 		return
 	}
-	e.RawJSON(op+".KeyValue", v0)
+	e.RawJSON(op, v)
 }
 
 func (p *Prefix) MarshalZerologObject(e *zerolog.Event) {
-	const op = op + ".Prefix.MarshalZerologObject"
-	v0, err := json.Marshal(&p)
+	const op = op + ".Prefix"
+	v, err := json.Marshal(&p)
 	if err != nil {
-		const op = op + ".json.Marshal"
+		const op = op + ".MarshalZerologObject.json.Marshal"
 		err := &errs.Error{Op: op, Code: codes.InvalidArgument, Err: err}
 		slog.Logger().Err(err).Str("op", op).Msg(err.Error())
 		return
 	}
-	e.RawJSON(op+".Prefix", v0)
+	e.RawJSON(op, v)
 }
--- service/event/zerolog.go
+++ service/event/zerolog.go
@@ -10,13 +10,13 @@ import (
 )
 
 func (p *Event) MarshalZerologObject(e *zerolog.Event) {
-	const op = op + ".Event.MarshalZerologObject"
-	v0, err := json.Marshal(&p)
+	const op = op + ".Event"
+	v, err := json.Marshal(&p)
 	if err != nil {
-		const op = op + ".json.Marshal"
+		const op = op + ".MarshalZerologObject.json.Marshal"
 		err := &errs.Error{Op: op, Code: codes.InvalidArgument, Err: err}
 		slog.Logger().Err(err).Str("op", op).Msg(err.Error())
 		return
 	}
-	e.RawJSON(op+".Event", v0)
+	e.RawJSON(op, v)
 }
