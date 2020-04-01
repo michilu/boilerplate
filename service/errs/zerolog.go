100b49e9a6209656738b492ef17d0506a8932966
ENDOH takanao <djmchl@gmail.com>
2020-04-01 14:19:55 +0900

    no nest error

--- service/errs/zerolog.go
+++ service/errs/zerolog.go
@@ -8,9 +8,6 @@ func (p *Error) MarshalZerologObject(e *zerolog.Event) {
 	e.
 		Str("message", p.Message).
 		Str("op", p.Op)
-	if p.Err != nil {
-		e.Err(p.Err)
-	}
 	if p.Code != nil {
 		e.Str("code", p.Code.String())
 	}
